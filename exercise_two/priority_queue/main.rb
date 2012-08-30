eb2ac28edc091eb15d00948d4df0620681028c47
Phil Schilter <phil.schilter@gmail.com>
2012-08-30 23:13:07 +0200

    add first version of priority queue

--- exercise_two/priority_queue/main.rb
+++ exercise_two/priority_queue/main.rb
@@ -3,8 +3,19 @@ require_relative 'priority_queue.rb'
 
 puts "*" * 20
 
-#priority_queue = Container::PriorityQueue.new(["EV Zug", 1], ["HC Ambri Piotta", 2])
-priority_queue = Container::PriorityQueue.new(["EV Zug", 1])
+priority_queue = Container::PriorityQueue.new(["EV Zug", 3], ["HC Ambri Piotta", 1], ["ZSC Lions", 2])
 
-binding.pry
+puts priority_queue
+
+puts "*" * 20
+
+puts priority_queue.inspect
+
+puts "*" * 20
+
+puts priority_queue.pop
+
+puts "*" * 20
+
+puts priority_queue.inspect
 
--- exercise_two/priority_queue/priority_queue.rb
+++ exercise_two/priority_queue/priority_queue.rb
@@ -1,4 +1,17 @@
 module Container
-  class PriorityQueue
+  class PriorityQueue < Array
+
+    def initialize(*args)
+      args.each{ |a| self.push a }
+    end
+
+    def push(val)
+      self << val
+      sort_by!{ |elem| elem[1] }
+    end
+
+    def inspect
+      self.map{ |elem| elem[0] }.join(', ')
+    end
   end
 end

00ed1d491ffa1a68aad890632a32af3b3249f5c9
Phil Schilter <philipp.schilter@screenconcept.ch>
2012-08-31 10:44:52 +0200

    update

--- exercise_two/priority_queue/main.rb
+++ exercise_two/priority_queue/main.rb
@@ -5,7 +5,7 @@ puts "*" * 20
 
 priority_queue = Container::PriorityQueue.new(["EV Zug", 3], ["HC Ambri Piotta", 1], ["ZSC Lions", 2])
 
-puts priority_queue
+puts "#{priority_queue}"
 
 puts "*" * 20
 
@@ -18,4 +18,3 @@ puts priority_queue.pop
 puts "*" * 20
 
 puts priority_queue.inspect
-
--- exercise_two/priority_queue/priority_queue.rb
+++ exercise_two/priority_queue/priority_queue.rb
@@ -8,6 +8,15 @@ module Container
     def push(val)
       self << val
       sort_by!{ |elem| elem[1] }
+      self
+    end
+
+    def pop
+      slice!(0)
+    end
+
+    def is_empty?
+      empty?
     end
 
     def inspect

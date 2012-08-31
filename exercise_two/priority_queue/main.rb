ebde789f0c9595aaff271f94563c36a00a16a3b3
Phil Schilter <philipp.schilter@screenconcept.ch>
2012-08-31 10:48:59 +0200

    update

--- exercise_two/priority_queue/main.rb
+++ exercise_two/priority_queue/main.rb
@@ -13,8 +13,22 @@ puts priority_queue.inspect
 
 puts "*" * 20
 
-puts priority_queue.pop
+puts "#{priority_queue.pop}"
 
 puts "*" * 20
 
 puts priority_queue.inspect
+
+puts "*" * 20
+
+puts priority_queue.size
+
+puts "*" * 20
+
+priority_queue.clear
+
+puts "Is it empty now?"
+puts "=> #{priority_queue.is_empty?}"
+
+puts "*" * 20
+
